98ab8c2a362a572296817f68824f94d52ad17ace
hehekmal <nezhadevil@gmail.com>
2026-05-06 11:59:11 +0700

    hikmal

---
+++ lib/pages/hikmal_loading_page.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class HikmalLoadingPage extends StatefulWidget {
+  const HikmalLoadingPage({super.key});
+
+  @override
+  State<HikmalLoadingPage> createState() => _HikmalLoadingPageState();
+}
+
+class _HikmalLoadingPageState extends State<HikmalLoadingPage> {
+  late Timer loadTime;
+  late Timer loadbool;
+  bool load = false;
+  @override
+  void initState() {
+    loadTime = Timer.periodic(Duration(seconds: 3), (timer) {
+      //Navigator.pushReplacement(
+        //context,
+        //MaterialPageRoute(),
+      //);
+    });
+    loadbool=Timer.periodic(Duration(milliseconds: 1), (timer) {
+      setState(() {
+        load = true;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    loadTime.cancel();
+    loadbool.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.indigo.shade900],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Aplikasi',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'Tugas Kelas',
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Image.asset('images/tulasap_logo.png', width: 150),
+            SizedBox(height: 40),
+            Container(
+              height: 10,
+              width: size.width * 0.7,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: 2500),
+                    height: 20,
+                    width: load ? size.width * 0.7 : 0,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              'Lengkapi tugas-tugas kalian yang belum tuntas',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
