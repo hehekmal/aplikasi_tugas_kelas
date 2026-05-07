443658e67162f38e6dae8470edc7ba83620c8a4a
chla imoet <chladiolanazwa@gmail.com>
2026-05-07 19:46:40 +0700

    chla

--- lib/pages/chla_login_page.dart
+++ lib/pages/chla_login_page.dart
@@ -8,6 +8,8 @@ class ChlaLoginPage extends StatefulWidget {
 }
 
 class _ChlaLoginPageState extends State<ChlaLoginPage> {
+  TextEditingController nama = TextEditingController();
+  TextEditingController password = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +18,61 @@ class _ChlaLoginPageState extends State<ChlaLoginPage> {
           width: double.infinity,
           height: double.infinity,
           padding: EdgeInsets.all(20),
-          decoration: BoxDecoration(color: Colors.blueAccent),
-
+          decoration: BoxDecoration(color: Colors.white),
+          child: Container(
+            width: double.infinity,
+            height: double.infinity,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(color: Colors.blue.shade800),
+            child: Column(
+              children: [
+                Text(
+                  "Login",
+                  style: TextStyle(fontSize: 80, color: Colors.white),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Text('Nama/Email:', style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+                TextField(
+                  controller: nama,
+                  decoration: InputDecoration(
+                    fillColor: Colors.grey.shade200,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text('Password:', style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+                TextField(
+                  controller: password,
+                  decoration: InputDecoration(
+                    fillColor: Colors.grey.shade200,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(children: [TextButton(onPressed: () {}, child: Text("daftar?", style: TextStyle(color: Colors.white),)), ElevatedButton(onPressed: () {}, child: Text("Login", style: TextStyle(color: Colors.white),),style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.blueAccent)),)],)
+              ],
+            ),
+          ),
         ),
       ),
     );
