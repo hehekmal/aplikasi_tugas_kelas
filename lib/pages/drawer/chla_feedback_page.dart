1fa60b4ca74d033876ada9bc41e234df4e7592a5
chla imoet <chladiolanazwa@gmail.com>
2026-05-11 08:32:11 +0700

    chla

--- lib/pages/drawer/chla_feedback_page.dart
+++ lib/pages/drawer/chla_feedback_page.dart
@@ -10,6 +10,52 @@ class ChlaFeedbackPage extends StatefulWidget {
 class _ChlaFeedbackPageState extends State<ChlaFeedbackPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        backgroundColor: Colors.black,
+        title: Text("Feedback", style: TextStyle(color: Colors.white)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "Berikan masukan anda",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "tulis disini...",
+                fillColor: Colors.grey,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Kirim", style: TextStyle(color: Colors.white)),
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.black),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
